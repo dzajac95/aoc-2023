8591a76d8420639888d8076526289e9071998195
dzajac <dzajac95@gmail.com>
2023-12-02 10:13:20 -0500

    day 2 starting point

---
+++ day2/src/part1.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// https://adventofcode.com/2023/day/2#part1
+
+pub fn run(input: []const u8) !void {
+    print("************\n", .{});
+    print("   Part 1\n", .{});
+    print("************\n", .{});
+    print("Input:\n", .{});
+    print("{s}\n", .{input});
+}
---
+++ day2/src/part2.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// https://adventofcode.com/2023/day/2#part2
+
+pub fn run(input: []const u8) !void {
+    print("************\n", .{});
+    print("   Part 2\n", .{});
+    print("************\n", .{});
+    print("Input:\n", .{});
+    print("{s}\n", .{input});
+}
