1e60d44a374d0e91518fed93c727c51e6b51f2db
dzajac <dzajac95@gmail.com>
2023-12-03 14:38:53 -0500

    day 3 starting point

---
+++ day3/src/part1.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// https://adventofcode.com/2023/day/2
+
+const total_red: u32 = 12;
+const total_green: u32 = 13;
+const total_blue: u32 = 14;
+
+pub fn run(input: []const u8) !void {
+    print("************\n", .{});
+    print("   Part 1\n", .{});
+    print("************\n", .{});
+    print("Input:\n{s}", .{input});
+}
