9a46cbfb79ba34dd6f93fac6ee6cb03c1ff3ea83
dzajac <dzajac95@gmail.com>
2023-12-06 23:24:52 -0500

    day 6 part 2 solved with MATH

--- day6/src/part2.zig
+++ day6/src/part2.zig
@@ -9,10 +9,38 @@ const ascii = std.ascii;
 pub fn run(input: []const u8) !void {
     var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = gpaObj.allocator();
-    _ = gpa;
     print("************\n", .{});
     print("   Part 2\n", .{});
     print("************\n", .{});
 
-    print("Input:\n{s}", .{input});
+    var tmpStr = std.ArrayList(u8).init(gpa);
+    var lines = mem.splitScalar(u8, input, '\n');
+
+    var line = lines.next().?;
+    var split = mem.splitScalar(u8, line, ':');
+    _ = split.next();
+    var valueStr = split.next().?;
+    var digits = mem.tokenizeScalar(u8, valueStr, ' ');
+    while (digits.next()) |digitSlice| {
+        try tmpStr.appendSlice(digitSlice);
+    }
+    const T = try fmt.parseFloat(f64, tmpStr.items);
+    print("Time: {d}\n", .{T});
+    tmpStr.clearAndFree();
+
+    line = lines.next().?;
+    split = mem.splitScalar(u8, line, ':');
+    _ = split.next();
+    valueStr = split.next().?;
+    digits = mem.tokenizeScalar(u8, valueStr, ' ');
+    while (digits.next()) |digitSlice| {
+        try tmpStr.appendSlice(digitSlice);
+    }
+    const d = try fmt.parseFloat(f64, tmpStr.items);
+    print("Distance: {d}\n", .{d});
+
+    var d1 = -1 * T + @sqrt(T * T - 4 * d) / -2;
+    var d2 = -1 * T - @sqrt(T * T - 4 * d) / -2;
+    const answer: usize = @intFromFloat(@floor(@fabs(d1 - d2)));
+    print("Answer: {d}\n", .{answer});
 }
