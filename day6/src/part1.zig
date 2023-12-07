e114cd9476144f5002bc60f029a221b8c5ac1ec3
dzajac <dzajac95@gmail.com>
2023-12-06 22:31:14 -0500

    day 6 part 1 done

--- day6/src/part1.zig
+++ day6/src/part1.zig
@@ -9,10 +9,48 @@ const ascii = std.ascii;
 pub fn run(input: []const u8) !void {
     var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
     const gpa = gpaObj.allocator();
-    _ = gpa;
     print("************\n", .{});
     print("   Part 1\n", .{});
     print("************\n", .{});
 
-    print("Input:\n{s}", .{input});
+    var lines = mem.tokenizeScalar(u8, input, '\n');
+    var times = std.ArrayList(usize).init(gpa);
+    var distRecords = std.ArrayList(usize).init(gpa);
+
+    var line = lines.next().?;
+    var split = mem.splitScalar(u8, line, ':');
+    _ = split.next();
+    var valueStr = split.next().?;
+    var values = mem.tokenizeScalar(u8, valueStr, ' ');
+    while (values.next()) |value| {
+        const valueNum = try fmt.parseInt(usize, value, 10);
+        try times.append(valueNum);
+    }
+    line = lines.next().?;
+    split = mem.splitScalar(u8, line, ':');
+    _ = split.next();
+    valueStr = split.next().?;
+    values = mem.tokenizeScalar(u8, valueStr, ' ');
+    while (values.next()) |value| {
+        const valueNum = try fmt.parseInt(usize, value, 10);
+        try distRecords.append(valueNum);
+    }
+
+    var answer: usize = 1;
+    for (times.items, distRecords.items) |time, record| {
+        print("{d}, {d}\n", .{ time, record });
+        var waysToBeat: usize = 0;
+        var vel: usize = 0;
+        var d: usize = 0;
+        for (0..time) |chargeTime| {
+            vel = 1 * chargeTime;
+            const remainingTime = time - chargeTime;
+            d = vel * remainingTime;
+            if (d > record) {
+                waysToBeat += 1;
+            }
+        }
+        answer *= waysToBeat;
+    }
+    print("Answer: {d}\n", .{answer});
 }
