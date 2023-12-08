8840595b3ad178639e6973dd4b17e58fe6e687c4
dzajac <dzajac95@gmail.com>
2023-12-07 22:30:42 -0500

    day 7 start!

---
+++ day7/src/part1.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const print = std.debug.print;
+const mem = std.mem;
+const fmt = std.fmt;
+const ascii = std.ascii;
+
+// https://adventofcode.com/2023/day/7
+
+pub fn run(input: []const u8) !void {
+    var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = gpaObj.allocator();
+    _ = gpa;
+    print("************\n", .{});
+    print("   Part 1\n", .{});
+    print("************\n", .{});
+    var lines = mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        print("{s}\n", .{line});
+    }
+}
