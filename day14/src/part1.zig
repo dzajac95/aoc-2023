af57cdf283190f36abf6aab6e9967285d8076ff9
dzajac <dzajac95@gmail.com>
2023-12-17 22:52:58 -0500

    day14 part1 done :)

--- day14/src/part1.zig
+++ day14/src/part1.zig
@@ -22,31 +22,68 @@ fn lol() void {
 }
 
 const Grid = struct {
-    data: []const u8,
+    data: []u8,
     w: usize,
     h: usize,
 
     const Self = @This();
-    pub fn fromChunk(input: []const u8) Self {
+
+    pub fn fromInput(input: []const u8) !Self {
         const w = mem.indexOfScalar(u8, input, '\n') orelse unreachable;
-        var iter = mem.tokenizeScalar(u8, input, '\n');
+        var lines = mem.tokenizeScalar(u8, input, '\n');
         var numRows: usize = 0;
-        while (iter.next()) |_| {
+        var tmp = std.ArrayList(u8).init(gpa);
+        defer tmp.deinit();
+
+        while (lines.next()) |line| {
             numRows += 1;
+            try tmp.appendSlice(line);
         }
         return Self{
-            .data = input,
+            .data = try tmp.toOwnedSlice(),
             .w = w,
             .h = numRows,
         };
     }
 
-    pub fn getIndex(self: Self, x: usize, y: usize) usize {
-        return y * self.w + y + x;
+    pub fn deinit(self: Self) void {
+        gpa.free(self.data);
+    }
+
+    pub fn index(self: Self, x: usize, y: usize) usize {
+        return y * self.w + x;
     }
 
     pub fn get(self: Self, x: usize, y: usize) u8 {
-        return self.data[self.getIndex(x, y)];
+        return self.data[self.index(x, y)];
+    }
+
+    pub fn set(self: *Self, x: usize, y: usize, c: u8) void {
+        self.data[self.index(x, y)] = c;
+    }
+
+    pub fn swap(self: *Self, x1: usize, y1: usize, x2: usize, y2: usize) void {
+        const tmp = self.get(x2, y2);
+        self.set(x2, y2, self.get(x1, y1));
+        self.set(x1, y1, tmp);
+    }
+
+    pub fn roll(self: *Self, x: usize, y: usize) void {
+        std.debug.assert(self.get(x, y) == 'O');
+        var currY: isize = @as(isize, @intCast(y)) - 1;
+        while (currY >= 0 and self.get(x, @intCast(currY)) == '.') {
+            self.swap(x, @intCast(currY + 1), x, @intCast(currY));
+            currY -= 1;
+        }
+    }
+
+    pub fn disp(self: Self) void {
+        for (0..self.h) |y| {
+            for (0..self.w) |x| {
+                print("{c}", .{self.get(x, y)});
+            }
+            print("\n", .{});
+        }
     }
 };
 
@@ -54,8 +91,24 @@ pub fn run(input: []const u8) !void {
     print("************\n", .{});
     print("   Part 1\n", .{});
     print("************\n", .{});
-    var lines = mem.tokenizeScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        print("{s}\n", .{line});
+    var grid = try Grid.fromInput(input);
+    defer grid.deinit();
+    for (0..grid.h) |y| {
+        for (0..grid.w) |x| {
+            if (grid.get(x, y) == 'O') {
+                grid.roll(x, y);
+            }
+        }
     }
+    var sum: usize = 0;
+    for (0..grid.h) |y| {
+        const value = grid.h - y;
+        var numRollers: usize = 0;
+        for (0..grid.w) |x| {
+            numRollers += if (grid.get(x, y) == 'O') 1 else 0;
+        }
+        sum += numRollers * value;
+    }
+    grid.disp();
+    print("Answer: {d}\n", .{sum});
 }
