1fd81b651fbd6a91aef0d78cb6fffdfd9026f404
dzajac <dzajac95@gmail.com>
2023-12-17 20:25:02 -0500

    day13 start

---
+++ day13/src/part2.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const print = std.debug.print;
+const assert = std.debug.assert;
+const mem = std.mem;
+const fmt = std.fmt;
+
+// https://adventofcode.com/2023/day/13#part2
+
+var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = gpaObj.allocator();
+
+fn contains(haystack: []const u8, needle: u8) bool {
+    for (haystack) |c| {
+        if (c == needle)
+            return true;
+    }
+    return false;
+}
+
+pub fn run(input: []const u8) !void {
+    print("************\n", .{});
+    print("   Part 2\n", .{});
+    print("************\n", .{});
+    var lines = mem.tokenizeScalar(u8, input, '\n');
+
+    while (lines.next()) |line| {
+        print("{s}\n", .{line});
+    }
+}
