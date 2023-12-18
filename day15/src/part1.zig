3bb29a684075d1d947d1fb4c1688e119f7c7e7d9
dzajac <dzajac95@gmail.com>
2023-12-18 02:44:26 -0500

    day15 part2 done

--- day15/src/part1.zig
+++ day15/src/part1.zig
@@ -9,13 +9,31 @@ const fmt = std.fmt;
 var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
 const gpa = gpaObj.allocator();
 
+fn hash(str: []const u8) usize {
+    var h: usize = 0;
+    for (str) |c| {
+        h = hashUpdate(h, c);
+    }
+    return h;
+}
+
+fn hashUpdate(h: usize, c: u8) usize {
+    var tmp = h;
+    tmp += c;
+    tmp = tmp * 17;
+    tmp = tmp % 256;
+    return tmp;
+}
+
 pub fn run(input: []const u8) !void {
     print("************\n", .{});
     print("   Part 1\n", .{});
     print("************\n", .{});
     const trimmed = mem.trim(u8, input, "\n");
-    var items = mem.tokenizeScalar(u8, trimmed, ',');
-    while (items.next()) |item| {
-        print("{s}\n", .{item});
+    var labels = mem.tokenizeScalar(u8, trimmed, ',');
+    var sum: usize = 0;
+    while (labels.next()) |label| {
+        sum += hash(label);
     }
+    print("Answer: {d}\n", .{sum});
 }
