0197d5974d2a5c3c0ab47499943edff660232345
dzajac <dzajac95@gmail.com>
2023-12-20 09:52:46 -0500

    day19 start

---
+++ day19/src/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const part1 = @import("part1.zig").run;
+const part2 = @import("part2.zig").run;
+
+const print = std.debug.print;
+const allocator = std.heap.page_allocator;
+
+fn readFile(file_path: []const u8) ![]u8 {
+    const path = try std.fs.realpathAlloc(allocator, file_path);
+    defer allocator.free(path);
+    const input_file = try std.fs.openFileAbsolute(path, .{});
+    defer input_file.close();
+    const stat = try input_file.stat();
+    const file_size = stat.size;
+    var input: []u8 = try allocator.alloc(u8, file_size);
+    _ = try input_file.read(input);
+    return input;
+}
+
+pub fn main() !void {
+    // Read in file
+    var args = std.process.args();
+    _ = args.next().?;
+    var filePath: []const u8 = undefined;
+    if (args.next()) |fp| {
+        filePath = fp;
+    } else {
+        print("ERROR: must provide file path to input!\n", .{});
+        std.os.exit(1);
+    }
+    const input = try readFile(filePath);
+    defer allocator.free(input);
+    if (args.next()) |subcmd| {
+        if (std.mem.eql(u8, subcmd, "part1")) {
+            try part1(input);
+        } else if (std.mem.eql(u8, subcmd, "part2")) {
+            try part2(input);
+        } else {
+            print("Unkown subcommand: {s}\n", .{subcmd});
+        }
+    } else {
+        try part1(input);
+        print("\n", .{});
+        try part2(input);
+    }
+}
---
+++ day19/src/part1.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const math = std.math;
+const mem = std.mem;
+const fmt = std.fmt;
+const fs = std.fs;
+const file = std.fs.File;
+
+const print = std.debug.print;
+const assert = std.debug.assert;
+
+// https://adventofcode.com/2023/day/19
+
+var gpaObj = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = gpaObj.allocator();
+
+fn contains(haystack: []const u8, needle: u8) bool {
+    for (haystack) |c| {
+        if (c == needle)
+            return true;
+    }
+    return false;
+}
+
+fn lol() void {
+    print("buy more ram lol\n", .{});
+    assert(false);
+}
+
+pub fn run(input: []const u8) !void {
+    print("************\n", .{});
+    print("   Part 1\n", .{});
+    print("************\n", .{});
+    var lines = mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        print("{s}\n", .{line});
+    }
+}
