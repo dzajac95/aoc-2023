22fe371ed1237351ba7c0b86c715c46836e0039c
dzajac <dzajac95@gmail.com>
2023-12-20 10:54:04 -0500

    day19 part1: implement parsing

--- day19/src/part1.zig
+++ day19/src/part1.zig
@@ -26,12 +26,109 @@ fn lol() void {
     assert(false);
 }
 
+const Category = enum { xcool, musical, aero, shiny, none };
+
+const Op = enum {
+    lt,
+    gt,
+    none,
+};
+
+const Rule = struct {
+    cat: Category = .none,
+    op: Op = .none,
+    value: usize = 0,
+    dest: []const u8 = "",
+};
+
+const PartRating = struct {
+    x: usize,
+    m: usize,
+    a: usize,
+    s: usize,
+};
+
+const Workflow = std.ArrayList(Rule);
+
+fn parseWorkflows(map: *std.StringHashMap(Workflow), str: []const u8) !void {
+    _ = map;
+    var lines = mem.tokenizeScalar(u8, str, '\n');
+    var wf = Workflow.init(gpa);
+    while (lines.next()) |line| {
+        const wfStart = mem.indexOfScalar(u8, line, '{').?;
+        const wfEnd = mem.indexOfScalar(u8, line, '}').?;
+        const id = line[0..wfStart];
+        _ = id;
+        const wfStr = line[wfStart + 1 .. wfEnd];
+        var rulesIter = mem.tokenizeScalar(u8, wfStr, ',');
+        while (rulesIter.next()) |ruleStr| {
+            var rule: Rule = .{};
+            if (mem.indexOfScalar(u8, ruleStr, ':')) |sepIdx| {
+                const left = ruleStr[0..sepIdx];
+                const right = ruleStr[sepIdx + 1 ..];
+                const catC = left[0];
+                const opC = left[1];
+                const valStr = left[2..];
+                rule.cat = switch (catC) {
+                    'x' => .xcool,
+                    'm' => .musical,
+                    'a' => .aero,
+                    's' => .shiny,
+                    else => unreachable,
+                };
+                rule.op = switch (opC) {
+                    '<' => .lt,
+                    '>' => .gt,
+                    else => unreachable,
+                };
+                rule.value = try fmt.parseInt(usize, valStr, 10);
+                rule.dest = right;
+            } else {
+                rule.dest = ruleStr;
+            }
+            try wf.append(rule);
+        }
+        wf.clearAndFree();
+    }
+}
+
+fn parseParts(list: *std.ArrayList(PartRating), str: []const u8) !void {
+    var lines = mem.tokenizeScalar(u8, str, '\n');
+    while (lines.next()) |line| {
+        const trimmed = mem.trim(u8, line, "{}");
+        var ratings = mem.splitScalar(u8, trimmed, ',');
+
+        const xStr = ratings.next().?;
+        const mStr = ratings.next().?;
+        const aStr = ratings.next().?;
+        const sStr = ratings.next().?;
+
+        const x = try fmt.parseInt(usize, xStr[2..], 10);
+        const m = try fmt.parseInt(usize, mStr[2..], 10);
+        const a = try fmt.parseInt(usize, aStr[2..], 10);
+        const s = try fmt.parseInt(usize, sStr[2..], 10);
+
+        try list.append(PartRating{
+            .x = x,
+            .m = m,
+            .a = a,
+            .s = s,
+        });
+    }
+}
+
 pub fn run(input: []const u8) !void {
     print("************\n", .{});
     print("   Part 1\n", .{});
     print("************\n", .{});
-    var lines = mem.tokenizeScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        print("{s}\n", .{line});
+    var chunks = mem.tokenizeSequence(u8, input, "\n\n");
+    const workflowStr = chunks.next().?;
+    const partsStr = chunks.next().?;
+    var workflowMap = std.StringHashMap(Workflow).init(gpa);
+    var partList = std.ArrayList(PartRating).init(gpa);
+    try parseWorkflows(&workflowMap, workflowStr);
+    try parseParts(&partList, partsStr);
+    for (partList.items) |part| {
+        print("part: {}\n", .{part});
     }
 }
